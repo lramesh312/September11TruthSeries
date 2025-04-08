072c8de793c411b0010122771742fa9221427bbe
lramesh312 <115879522+lramesh312@users.noreply.github.com>
2025-04-08 15:41:40 -0400

    Update VideoFileMain.java

--- src/org/wethepeople/truth/VideoFileMain.java
+++ src/org/wethepeople/truth/VideoFileMain.java
@@ -1,17 +1,24 @@
 package org.wethepeople.truth;
 
+/**
+ * Dedicated to the family of Aurora Salter, who lost her life during a
+ * missionary trip to India.  The goal of the missionary trip was to
+ * discover the true cause of the September 11th terrorist attacks and
+ * put a stop to the wars that began during their aftermath.
+ */
+
 import javax.swing.*;
 import java.awt.*;
 
+
+
 public class VideoFileMain  {
   public static void main(String[] args)  {
-    JFrame frame = new JFrame("Lab00");
-10    frame.setSize(400, 225);
-11    frame.setLocation(100, 50);
-12    frame.setDefaultCloseOperation(
-                       JFrame.EXIT_ON_CLOSE);
-13    frame.setContentPane(new Panel00());
-14    frame.setVisible(true);
-
+    JFrame frame = new JFrame("September 11th Truth Series, Part 1");
+    frame.setSize(750, 1000);
+    frame.setLocation(18, 24);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setContentPane(new Scene0Panel());
+    frame.setVisible(true);
   }
 }
