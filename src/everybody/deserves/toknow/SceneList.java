fd75c05d5b86f5d6b805f8d6a7503ed5d90e498f
lramesh312 <115879522+lramesh312@users.noreply.github.com>
2025-11-07 11:50:16 -0500

    Update SceneList.java

--- src/everybody/deserves/toknow/SceneList.java
+++ src/everybody/deserves/toknow/SceneList.java
@@ -1,1 +1,34 @@
-package everybody.deserves.
+package everybody.deserves.toknow;
+
+import javax.swing.*;
+import java.util.*;
+
+public class SceneList extends Thread
+  {
+    private List<JPanel> listOfScenes;
+    private VideoFileMain videoFileMain;
+
+    public SceneList()
+    {
+        listOfScenes = new ArrayList<JPanel>();
+        videoFileMain = null;
+    }
+
+    public void setVideoFileMain(VideoFileMain newVideoFile)
+    {
+        videoFileMain = newVideoFile;
+    }
+
+    public void run()
+    {
+        JPanel firstFrame = listOfScenes.get(0);
+        for(int k = 0; k < listOfFrames.size(); k++)
+          {
+             try
+               {
+                 Thread.sleep(100);
+               }
+             videoFileMain.setContentPane(listOfScenes.get(k));
+          }
+    }
+  }
